3c611481e1f7f64e32d6502e152cff6aa2a58277
maneesh <maneesh.kammari@gmail.com>
2024-06-08 15:17:48 +0530

    done

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    
+const nextConfig = {
+  reactStrictMode: true,
+  exportPathMap: async function (
+    _defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' }
+      // specify other paths here if needed
+    };
   }
+};
 
-  module.exports = nextConfig
+module.exports = nextConfig;
+
